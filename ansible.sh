1129b01ec5e423ee31390ffd3a06100b623f1d43
Darlan Junior <darlan.sbjr@gmail.com>
2017-06-19 12:55:18 -0300

    Build docker image and ensure AWS credentials are present

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+if [[ "$(docker images -q ansible-boto:latest 2> /dev/null)" == "" ]]
+then
+  docker build -t ansible-boto:latest .
+fi
+
+if ! [[ -d .aws && -f .aws/credentials && -f .aws/config ]]
+then
+  echo "Please place your AWS credentials on $(pwd)/.aws"
+  exit
+fi
+
 docker run --rm -v $(pwd):/root/ -it ansible-boto /bin/sh
+
